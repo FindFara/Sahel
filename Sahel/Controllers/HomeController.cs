9fccd466e7864f27601e3d8ae59c8aa608d25c20
Hosein Khakpour <h.khakpour@asax.ir>
2026-06-07 09:31:18 +0330

    add svg

--- Sahel/Controllers/HomeController.cs
+++ Sahel/Controllers/HomeController.cs
@@ -6,8 +6,41 @@ namespace Sahel.Controllers
 {
     public class HomeController : Controller
     {
+        private const string LoginName = "Chihiro";
+        private const string LoginSessionKey = "LoginName";
+
         public IActionResult Index()
         {
+            if (HttpContext.Session.GetString(LoginSessionKey) != LoginName)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetString(LoginSessionKey) == LoginName)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(string name)
+        {
+            if (string.Equals(name?.Trim(), LoginName, StringComparison.OrdinalIgnoreCase))
+            {
+                HttpContext.Session.SetString(LoginSessionKey, LoginName);
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(nameof(name), "Name must be Chihiro.");
             return View();
         }
 
